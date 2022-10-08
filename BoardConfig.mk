465efbbe9a91c4cbbf6de6b26dd51d59052c9e90
Mateo Buteler <mateojosebuteler@gmail.com>
2022-10-08 20:38:57 -0300

    libinit changes for A12
    Change-Id: I3eca489f42fdde4315047a70d134d5189e410959

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,5 @@ TARGET_OTA_ASSERT_DEVICE := starqltechn
 TARGET_KERNEL_CONFIG := starqlte_chn_open_defconfig
 
 # Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_starqltechn
-TARGET_RECOVERY_DEVICE_MODULES := libinit_starqltechn
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_samsung_starqltechn
+TARGET_RECOVERY_DEVICE_MODULES := init_samsung_starqltechn
